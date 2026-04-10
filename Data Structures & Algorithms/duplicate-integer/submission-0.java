41d6ee588dca994cf83bb1953b4fb627be2d5318
ZeelModi4 <lancersfree484@gmail.com>
2026-04-10 00:17:56 -0500

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int i = 0; i < nums.length; i++){
+            if(map.get(nums[i]) == null){
+                map.put(nums[i], i);
+            }else{
+                return true;
+            }
+        }
+
+        return false;
+
+    }
+}
